e7f2f5c9c27c7781f3cdde01ebc21aee56dc5d8a
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2018-07-05 12:41:25 +0200

    WordPress coding standard - third batch (#2)

--- genesis-ads.php
+++ genesis-ads.php
@@ -17,29 +17,28 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// only load if not already existing (maybe within another plugin I created)
-if(!class_exists('Advanced_Ads_Genesis_Plugin')) {
+// Only load if not already existing (maybe within another plugin I created).
+if ( ! class_exists( 'Advanced_Ads_Genesis_Plugin' ) ) {
 
-// load basic path and url to the plugin
-define('AAG_BASE_PATH', plugin_dir_path(__FILE__));
-define('AAG_BASE_URL', plugin_dir_url(__FILE__));
-define('AAG_BASE_DIR', dirname( plugin_basename( __FILE__ ) ) ); // directory of the plugin without any paths
+	// Load basic path and url to the plugin.
+	define( 'AAG_BASE_PATH', plugin_dir_path( __FILE__ ) );
+	define( 'AAG_BASE_URL', plugin_dir_url( __FILE__ ) );
+	define( 'AAG_BASE_DIR', dirname( plugin_basename( __FILE__ ) ) ); // Directory of the plugin without any paths.
 
-// plugin slug and textdoamin
-define('AAG_SLUG', 'advanced-ads-genesis');
+	// Plugin slug and textdomain.
+	define( 'AAG_SLUG', 'advanced-ads-genesis' );
 
-define( 'AAG_VERSION', '1.0.4' );
-define( 'AAG_PLUGIN_URL', 'https://wpadvancedads.com' );
-define( 'AAG_PLUGIN_NAME', 'Genesis Ads' );
+	define( 'AAG_VERSION', '1.0.4' );
+	define( 'AAG_PLUGIN_URL', 'https://wpadvancedads.com' );
+	define( 'AAG_PLUGIN_NAME', 'Genesis Ads' );
 
-include_once( plugin_dir_path( __FILE__ ) . 'classes/plugin.php' );
+	include_once plugin_dir_path( __FILE__ ) . 'classes/plugin.php';
 
-if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-    include_once( plugin_dir_path( __FILE__ ) . 'admin/admin.php' );
-    new Advanced_Ads_Genesis_Admin();
-} else {
-    include_once( plugin_dir_path( __FILE__ ) . 'public/public.php' );
-    new Advanced_Ads_Genesis();
-}
-
+	if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+		include_once plugin_dir_path( __FILE__ ) . 'admin/admin.php';
+		new Advanced_Ads_Genesis_Admin();
+	} else {
+		include_once plugin_dir_path( __FILE__ ) . 'public/public.php';
+		new Advanced_Ads_Genesis();
+	}
 }
